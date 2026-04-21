1d91cb7983c75a56ed47c05dfe8f96cd3d124179
FabianSoriano8 <N00273051@upn.pe>
2026-04-21 15:39:28 -0500

    Remove unused `androidx.compose.ui.semantics.text` import in `LoginActivity.kt`.

--- app/src/main/java/com/example/appcineindie/login/LoginActivity.kt
+++ app/src/main/java/com/example/appcineindie/login/LoginActivity.kt
@@ -5,7 +5,6 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.ui.semantics.text
 import com.example.appcineindie.R
 
 
